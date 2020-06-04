575c7b9c7a0caedf1169962df3e46b399c384537
foxx3r <coluna123@protonmail.ch>
2020-06-04 01:26:40 +0000

    fixed link error

--- zsh_config.sh
+++ zsh_config.sh
@@ -1,2 +1,2 @@
-wget https://raw.githubusercontent.com/foxx3r/my_zsh_config/master/tools/.zshrc ~/ 
+wget https://raw.githubusercontent.com/foxx3r/my_zsh_config/master/.zshrc ~/ 
 sh -c "$(wget -O- https://raw.githubusercontent.com/foxx3r/my_zsh_config/master/install.sh)"
