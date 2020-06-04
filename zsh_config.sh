d5bfe8be0f43d1797ced5ba3e3e80ae7e29ff3c9
foxx3r <coluna123@protonmail.ch>
2020-06-03 23:32:39 -0300

    added shebang and fixed error

--- zsh_config.sh
+++ zsh_config.sh
@@ -1,2 +1,3 @@
-wget https://raw.githubusercontent.com/foxx3r/my_zsh_config/master/.zshrc ~/ 
+#!/bin/bash
+wget https://raw.githubusercontent.com/foxx3r/my_zsh_config/master/.zshrc -O $HOME/.zshrc 
 sh -c "$(wget -O- https://raw.githubusercontent.com/foxx3r/my_zsh_config/master/install.sh)"
