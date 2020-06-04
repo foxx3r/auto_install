3fd29afac05e32d18d2db23b445a960093a865bf
foxx3r <coluna123@protonmail.ch>
2020-06-03 23:35:58 -0300

    added make and cmake

--- main.sh
+++ main.sh
@@ -20,7 +20,7 @@ then
         echo "foxxer    ALL=(ALL:ALL) ALL" >> /etc/sudoers
     fi
     apt install python3-pip npm clisp sbcl -y
-    apt install swi-prolog gnupg npm xz-utils ruby-dev gforth python bat curl wget ruby clang zsh git elixir neovim nodejs nano -y
+    apt install swi-prolog gnupg make cmake npm xz-utils ruby-dev gforth python bat curl wget ruby clang zsh git elixir neovim nodejs nano -y
     git config --global user.email "coluna123@protonmail.ch"
     git config --global user.name "foxx3r"
     npm config set strict-ssl false
@@ -88,7 +88,7 @@ if [[ $choose == "5" ]]
 then
     apt update
     apt upgrade -y
-    apt install python3 nodejs bat swi-prolog zsh curl wget ruby zsh git elixir neovim gnupg emacs pforth -y
+    apt install python3 make cmake nodejs bat swi-prolog zsh curl wget ruby zsh git elixir neovim gnupg emacs pforth -y
     echo "deb https://its-pointless.github.io/files termux extras" >> $PREFIX/etc/apt/sources.list
     wget https://its-pointless.github.io/pointless.gpg
     apt-key add pointless.gpg
