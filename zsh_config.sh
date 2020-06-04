d861cf8f89adc947648525e2099d3fa4b423b48a
foxx3r <coluna123@protonmail.ch>
2020-06-03 23:22:48 -0300

    removed gem install

--- zsh_config.sh
+++ zsh_config.sh
@@ -1,3 +1,2 @@
-gem install colorls
 wget https://raw.githubusercontent.com/foxx3r/my_zsh_config/master/.zshrc ~/ 
 sh -c "$(wget -O- https://raw.githubusercontent.com/foxx3r/my_zsh_config/master/install.sh)"
